6e3941cea8beec47ff553c3992df142e68367b13
Ahmet ATAY <ahmet.atay@hotmail.com>
2020-02-18 21:17:02 +0300

    TSLint fix

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,55 @@
 export default class Subject {
-  private _paused: boolean = false;
-  private _cancelled: boolean = false;
-  private _onCompCb?: CallableFunction;
-  private _subscribers: Array<[Function, Function | undefined] | []> = [];
+  private paused: boolean = false;
+  private cancelled: boolean = false;
+  private onCompCb?: CallableFunction;
+  private subscribers: Array<[CallableFunction, CallableFunction | undefined] | []> = [];
 
   public subscribe(observer: CallableFunction, err?: CallableFunction) {
-    this._subscribers.push([observer, err]);
-    const lastIndex = this._subscribers.length - 1;
-    const unsubscribe = () => (this._subscribers[lastIndex] = []);
+    this.subscribers.push([observer, err]);
+    const lastIndex = this.subscribers.length - 1;
+    const unsubscribe = () => (this.subscribers[lastIndex] = []);
     return {
       unsubscribe,
     };
   }
 
-  public async notify(data?: any): Promise<any | undefined> {
-    if (this._paused === true) {
+  public async notify(parameter?: any): Promise<any | undefined> {
+    if (this.paused === true) {
       return;
     }
 
-    let handlers;
     let index = 0;
 
-    if (this._cancelled === true) {
-      this._cancelled = false;
+    if (this.cancelled === true) {
+      this.cancelled = false;
       return;
     }
 
     const stack: Array<Promise<any>> = [];
 
-    startLabel: while ((handlers = this._subscribers[index])) {
+    startLabel: while (this.subscribers[index]) {
+      const [success, error] = this.subscribers[index];
       index++;
-
-      const [success, error] = handlers;
 
       if (!success) {
         continue startLabel;
       }
 
-      const promise = new Promise(resolve => resolve(data));
+      const promise = new Promise(resolve => resolve(parameter));
       const process = promise.then(data => success(data)).catch(e => error && error(e));
       stack.push(process);
     }
 
-    return Promise.all(stack).then(() => this._onCompCb && this._onCompCb());
+    return Promise.all(stack).then(() => this.onCompCb && this.onCompCb());
   }
 
-  public pause = () => (this._paused = true);
-  public resume = () => (this._paused = false);
-  public cancel = () => (this._cancelled = true);
+  public pause = () => (this.paused = true);
+  public resume = () => (this.paused = false);
+  public cancel = () => (this.cancelled = true);
   public reset = () => {
-    this._subscribers = [];
-    this._onCompCb = undefined;
+    this.subscribers = [];
+    this.onCompCb = undefined;
   };
-  public size = () => this._subscribers.filter(sub => sub.length > 0).length;
-  public onCompleted = (cb: CallableFunction) => (this._onCompCb = cb);
+  public size = () => this.subscribers.filter(sub => sub.length > 0).length;
+  public onCompleted = (cb: CallableFunction) => (this.onCompCb = cb);
 }
